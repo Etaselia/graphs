72a6aa56742f6b087ccca44a88dddc9c3a43babd
Simon Ptak <progamersp2@gmail.com>
2023-03-29 16:04:15 +0200

    Initial Upload

---
+++ src/test/java/MatrixTest.java
@@ -0,0 +1,24 @@
+import net.eta.Matrix;
+import net.eta.MatrixException;
+
+public class MatrixTest {
+    public static void main(String[] args) {
+        Matrix m1 = new Matrix(5,5,true);
+        try {
+            /* m1.saveCsv("./output/file.csv");
+            m1.print();
+            Matrix m2 = Matrix.loadCsv("./output/file.csv");
+            m2.print();*/
+            /* m1.distance().print();
+            m1.eccentricity().print();*/
+            m1.distance().print();
+            m1.diameter().print();
+            m1.radius().print();
+            m1.diameter().print();
+            m1.center().print();
+
+        } catch (MatrixException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
